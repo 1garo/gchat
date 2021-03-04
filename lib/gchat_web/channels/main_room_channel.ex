9bdb4e382ffc4fc616ad58058bc320a5107f01fb
1garo <alevardai@hotmail.com>
2021-03-04 20:08:27 -0300

    feat: missing name validation, asked a question to aleDsz

--- lib/gchat_web/channels/main_room_channel.ex
+++ lib/gchat_web/channels/main_room_channel.ex
@@ -1,6 +1,6 @@
 defmodule GchatWeb.MainRoomChannel do
   use GchatWeb, :channel
-
+  require Logger
   alias Gchat.Chats
 
   @impl true
@@ -12,6 +12,16 @@ defmodule GchatWeb.MainRoomChannel do
   # broadcast to everyone in the current topic (main_room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Logger.debug "entrou" 
+    Chats.create_message(payload)
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  #TODO: validate payload if name == ""
+  @impl true
+  def handle_in("shout", payload, socket) do
+    Logger.debug "se vier name vazio, setar %{name: 'user'}" 
     Chats.create_message(payload)
     broadcast socket, "shout", payload
     {:noreply, socket}
