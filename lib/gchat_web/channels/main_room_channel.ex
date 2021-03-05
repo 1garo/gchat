e39b228a0ead6db51e4b6525063738b8aa348dcb
1garo <alevardai@hotmail.com>
2021-03-05 20:16:34 -0300

    feat: solve name field validation on handle_in fn

--- lib/gchat_web/channels/main_room_channel.ex
+++ lib/gchat_web/channels/main_room_channel.ex
@@ -11,20 +11,15 @@ defmodule GchatWeb.MainRoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (main_room:lobby).
   @impl true
+  def handle_in("shout", payload = %{"name" => name}, socket) when name == "" or is_nil(name) do
+    Logger.debug "entrou aqui"
+    handle_in("shout", %{payload | "name" => "user"}, socket)
+  end
+
   def handle_in("shout", payload, socket) do
     Logger.debug "entrou" 
     Chats.create_message(payload)
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
-
-  #TODO: validate payload if name == ""
-  @impl true
-  def handle_in("shout", payload, socket) do
-    Logger.debug "se vier name vazio, setar %{name: 'user'}" 
-    Chats.create_message(payload)
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
-
 end
